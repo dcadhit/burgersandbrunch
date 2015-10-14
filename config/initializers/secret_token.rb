a52cb8d162fb279bc30c99c77f1730b45f8b0ebc
Danielle Cadhit <dcadhit@outlook.com>
2015-10-14 02:03:51 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Burgersbrunch::Application.config.secret_key_base = '3d912ad8a0e345625d49b5fc967c63a402b946edbe402e4c1f805a607bbd8dee6f8508e160e99cbefcc453c24e85ac59d03a094ffe7c1df3f893b50cc7a04549'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Burgersbrunch::Application.config.session_store :cookie_store, key: '_burgersbrunch_session'
