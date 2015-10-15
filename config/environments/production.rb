1dc01d1352449b8cfca17b6e922d489f17d05435
Danielle Cadhit <dcadhit@outlook.com>
2015-10-15 02:35:40 +0000

    Set up devise gem for user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Burgersbrunch::Application.configure do
+    config.action_mailer.default_url_options = { :host => 'burgers-n-brunch.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Burgersbrunch::Application.routes.draw do
 
+  devise_for :users
   root 'places#index'
 
   resources :places
